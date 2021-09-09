cdb059376bbf530b31e1f3101f433561f3de0820
daltonfranco <daltonpacolafranco.git@hotmail.com>
2021-09-09 14:00:41 -0300

    alteracoes do exercicio3 da semana 5 de java. For encadeado

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
         Encadeado encad = new Encadeado();
 
         encad.encadeado();
